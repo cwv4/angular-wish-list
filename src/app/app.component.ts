0ff229b87b63eb6a18b40e930f5095ec25587903
Caleb Warner <cwarner@vertafore.com>
2025-08-29 14:18:23 -0400

    Added directions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,4 +17,7 @@ export class AppComponent {
     new WishItem('Have coffee', true),
     new WishItem('Find grass that mows itself')
   ];
+
+  showDirections:boolean = true;
+  directions:string = 'Please check wishes off list as they become fulfilled';
 }
